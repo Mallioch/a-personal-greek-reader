03ed2c86dfafc299b139f1b698eb607e74c18f1e
Eric Sowell <ericsowell@Erics-MBP.home>
2015-10-03 13:10:50 -0500

    removed shell file

--- build.sh
+++
@@ -1,7 +0,0 @@
-
-xelatex '\def\treadmill{foo} \input{personal_greek_reader.tex}'
-mv personal_greek_reader.pdf reader_treadmill.pdf
-xelatex '\input{personal_greek_reader.tex}'
-mv personal_greek_reader.pdf reader_normal.pdf
-
-open reader_normal.pdf
